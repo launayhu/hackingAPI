50cd218e674766eaf42bc78393f9a4b82877f865
launayhu <89541752+launayhu@users.noreply.github.com>
2022-10-27 21:59:42 -0400

    Add files via upload

--- API_hack.py
+++ API_hack.py
@@ -34,6 +34,7 @@ def hello_world():
         
 if __name__ == "__main__":
     #with local IP
-    app.run(host='0.0.0.0', port=5000)
+    #app.run(host='0.0.0.0', port=5000)
+    app.run(host='localhost', port=5000)
     #with local host
     #app.run()
